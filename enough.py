dafae48a04ca34808f95e34d11c3a9bed5d71230
byfurkan00 <135124958+byfurkan00@users.noreply.github.com>
2025-07-14 21:04:52 +0300

    Update enough.py

--- enough.py
+++ enough.py
@@ -15,16 +15,27 @@ for attribute in dir(SendSms):
 while 1:
     system("cls||clear")
     print("""{}
-     ______                         _     
-    |  ____|                       | |    
-    | |__   _ __   ___  _   _  __ _| |__  
-    |  __| | '_ \ / _ \| | | |/ _` | '_ \ 
-    | |____| | | | (_) | |_| | (_| | | | |
-    |______|_| |_|\___/ \__,_|\__, |_| |_|
-                               __/ |      
-                              |___/      
+    ,        ,
+              /(        )`
+              \ \___   / |
+              /- _  `-/  '
+             (/\/ \ \   /\    
+             / /   | `    \
+             O O   ) /    |
+             `-^--'`<     '
+            (_.)  _ )    /
+             `.___/`    /
+               `-----' /
+  <----.     __ / __   \
+  <----|====O)))==) \) /====
+  <----'    `--' `.__,' \
+               |        |
+                \       /
+           ______( (_  / \______
+         ,'  ,-----'   |        \
+         `--{__________)        \/
     
-    Sms: 49{}           {}coder {}byfurkan\n  
+    Sms: 49{}           {}coder {}@mustafa.enes23\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (Normal)\n\n 2- SMS Gönder (Turbo😈)\n\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
